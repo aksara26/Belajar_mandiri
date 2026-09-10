2817ccf6e110524b7e868401a7c81043488e3570
aksara26 <aksarabeta26@gmail.com>
2026-09-10 14:04:11 +0700

    Hari_1_variabel

---
+++ Hari_1,1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int nim;
+    string nama,jurusan;
+    cout<<"Masukkan NIM : ";cin>>nim;
+    cout<<"Masukkan Nama : ";cin>>nama;
+    cout<<"Masukkan Jurusan : ";cin>>jurusan;
+    cout<<nim<<endl;
+    cout<<nama<<endl;
+    cout<<jurusan<<endl;
+    return 0;
+}
