a235cee99bf94c2c293220fe5afe7ac7b99bb466
aksara26 <aksarabeta26@gmail.com>
2026-09-11 14:42:17 +0700

    Hapus Hari_2.cpp

---
+++ Hari_2,3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Masukkan angka :";
+    cin>>a;
+    if(a%2==0){
+        cout<<"Genap";
+    }
+    else{
+        cout<<"Ganjil";
+    }
+    return 0;
+}
