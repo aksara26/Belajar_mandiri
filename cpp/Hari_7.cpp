63e52501f0004c80e9fb12adf4d19f2ff153ca15
aksara26 <aksarabeta26@gmail.com>
2026-09-16 20:21:12 +0700

    Error

---
+++ cpp/Hari_7.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main(){
+int password;
+int kesempatan=0;
+while(kesempatan>0){
+    cout<<"masukkan password"<<endl;
+    cin>>password;
+}
+if(password=76){
+    cout<<"Password Benar";
+    break;
+}
+else{
+    kesempatan--:
+    cout<<"Password salah"<<endl;
+    cout<<"Sisa Kesempatan :"<<kesempatan<<endl;
+
+}
+if(kesempatan==0){
+    cout<<"Akun anda terkunci";
+
+}
+return 0;
+}
+
+
+
+
+
+
+
+
+    return 0;
+})
