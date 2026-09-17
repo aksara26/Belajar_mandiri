4eca7a957d07e27a0397d7d16f2c61b0184f147d
aksara26 <aksarabeta26@gmail.com>
2026-09-18 01:06:48 +0700

    hari_8

---
+++ cpp/Hari_8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    float pagi = 2.4 ;
+    float siang = 1.5 ;
+    float malam = 0.7 ;
+    int total (pagi+siang+malam);
+
+    cout << total << endl;
+    cout << "Apakah sudah melebihi dualiter? " << total << endl;
+    return 0;
+
+}
