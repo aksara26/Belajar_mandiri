4a0d2dbd7e4492228d40ef43785ab8ba85dc4867
aksara26 <aksarabeta26@gmail.com>
2026-09-20 15:31:37 +0700

    bingung mau bikin apa Bulan bulan

---
+++ cpp/Hari_10.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main(){
+int Bulan;
+cin>>Bulan;
+if(Bulan=1){
+    cout<<"Januari";
+}
+else if(Bulan=2){cout<<"Februari";
+}
+else if(Bulan=3){cout<<"Maret";
+}else if(Bulan=4)
+{cout<<"April";
+}else if(Bulan=5)
+{cout<<"Mei";
+}else if(Bulan=6)
+{cout<<"Juni";
+}else if(Bulan=7)
+{cout<<"Juli";
+}else if(Bulan=8)
+{cout<<"Agustus";
+}else if(Bulan=9)
+{cout<<"September";
+}else if(Bulan=10)
+{cout<<"Oktober";
+}else if(Bulan=11)
+{cout<<"November";
+}else if(Bulan=12)
+{cout<<"Desember";
+}
+
+
+
+
+
+    return 0;
+}
